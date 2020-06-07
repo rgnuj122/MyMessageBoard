aebf866f38d854ca7bfe44c42b942ff9c8afd45c
mathilda <mathilda@honeywld.com.tw>
2020-06-07 23:15:05 +0800

    add message class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Message } from './message'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,51 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'MyMessageBoard';
+  /**
+  * 綁定畫面中的「名稱」欄位
+  *
+  * @memberof AppComponent
+  */
+  name = '';
+
+  /**
+   * 綁定畫面中的「內容」欄位
+   *
+   * @memberof AppComponent
+   */
+  content = '';
+  /**
+   * 所有留言都放在這裡
+   *
+   * @type {Message[]}
+   * @memberof AppComponent
+   */
+  messages: Message[] = [];
+
+  /**
+   * 新增留言
+   *
+   * @memberof AppComponent
+   */
+  addMessage(): void {
+
+    // 防呆，避免名稱或內容是空值時也可以留言
+    if (
+      !this.name.trim() ||
+      !this.content.trim()
+    ) {
+      return;
+    }
+
+    // 用名稱跟內容產生一個留言的資料物件
+    const message = new Message(this.name, this.content);
+
+    // 將留言的資料物件放進容器裡
+    this.messages.push(message);
+    // console.log(message.date);
+
+    // 清空內容
+    this.content = '';
+  }
 }
+
